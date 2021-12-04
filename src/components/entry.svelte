72dd4cd2977095353e8668c763abb3a0a5024557
Thomasorus <contact@thomasorus.com>
2021-12-04 19:50:49 +0100

    Stuff

---
+++ src/components/entry.svelte
@@ -0,0 +1,61 @@
+<script>
+	export let data;
+	let entr = data
+		const url = "http://localhost:3001/"
+
+	async function loadContent (el) {
+		const res = await fetch(`${url}fetchContent/${el}`);
+		let entry = await res.json();
+
+		if(entry.entry !== undefined) {
+			entr.content = entry.entry[0].content
+		} else {
+			entr.content = "Cet article est résevé aux premiums. Abonnez-vous !"
+		}
+	}
+</script>
+
+<article>
+	<img loading="lazy" src={entr.image} alt={entr.title}>
+	<h2>{entr.title}</h2>
+	<span>Par : {entr.creator}</span> le <time>{entr.pubDate}</time>
+	<p>{entr.description}</p>
+	<a href={entr.link}>Voir sur GK</a>
+	 <details>
+		  <summary on:click={loadContent(entr.id)}>Charger l'article</summary>
+		  <div class="details">
+		  	{@html entr.content}
+		  </div>
+	</details>
+</article>
+
+<style global>
+	.details {
+		padding: 1rem;
+		border: 1px solid var(--c4);
+	}
+	article > *+*, details > *+* {
+		margin-top: 1em;
+	}
+	article {
+		max-width: 80ch;
+		margin: auto;
+		padding: 2em;
+				margin-bottom: 1em;
+
+	}
+
+	summary {
+		background-color: #191919;
+		color: white;
+		padding: 0.5em;
+		border: none;
+		border-radius: 1em;
+		display: inline-block;
+	}
+
+	.details *+* {
+		margin-top: 1em;
+	}
+
+</style>
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,74 +1,30 @@
 <script>
+	import  Entry from '../components/entry.svelte'
 	import { onMount } from 'svelte';
-	const url = "https://gkapi.thomasorus.com"
-	let data
+	// const url = "https://gkapi.thomasorus.com"
+	const url = "http://localhost:3001/"
+
+	let data = undefined
 
 	onMount(async () => {
 		const res = await fetch(url);
 		data = await res.json();
-		// data.sort(function(a,b){
-		//   return new Date(b.pubDate) - new Date(a.pubDate);
-		// });
 	});
 
-	function loadContent (el) {
-		console.log(el)
-	}
 </script>
 
-<article class="flow-2">
 	{#if data === undefined}
 	       <h2> Loading Data...</h2>
 	{:else}
-		<ul class="flow-2">
+
 	    {#each data.entries as entr}
-	    	<li class="flow-1">
-	    		<img src={entr.image} alt={entr.title}>
-	    		<h2>{entr.title}</h2>
-	    		<span>Par : {entr.creator}</span> le <time>{entr.pubDate}</time>
-	    		<p>{entr.description}</p>
-	    		<button on:click={loadContent}>Charger l'article</button>
-	    		<a href={entr.link}>Voir sur GK</a>
-	    	</li>
-
+	    	<Entry data={entr}/>
 	    {/each}
-	    </ul>
 	{/if}
-</article>
 
-<style>
-	article {
-		max-width: 100ch;
-		margin: auto;
-		padding: 2em;
-	}
-	li {
-		position: relative;
-	}
-	img {
-		max-width: 100%;
-	}
-	.absolute-link {
-		position: absolute;
-		top: 0;
-		left: 0;
-		width: 100%;
-		height: 100%;
-	}
+<style global>
 
-	.flow-1 > *+* {
-		margin-top: 1em;
-	}
-	.flow-2 > *+* {
-		margin-top: 2em;
-	}
-	button {
-		background-color: #191919;
-		color: white;
-		padding: 0.5em;
-		border: none;
-		border-radius: 1em;
-	}
+
 	a {
 		text-decoration: underline;
 	}
